bc861e606ea3b1624ded81b46ff2e99ec6cc4f68
Prith Jaganathan <58461828+prith98@users.noreply.github.com>
2022-03-03 17:11:23 -0800

    Time: 190 ms (49.45%), Space: 66.8 MB (22.32%) - LeetHub

--- 219-contains-duplicate-ii/219-contains-duplicate-ii.js
+++ 219-contains-duplicate-ii/219-contains-duplicate-ii.js
@@ -6,33 +6,14 @@
 var containsNearbyDuplicate = function(nums, k) {
     
     if (nums.length === 1) return false;
-    
-    let numObj = {}
-    
+
+    let map = new Map();
     for (let i = 0; i < nums.length; i++) {
-        if (numObj[nums[i]] === undefined) {
-            numObj[nums[i]] = [i];
-        } else {
-            numObj[nums[i]].push(i);
-        }
+    if (i - map.get(nums[i]) <= k) {
+      return true;
     }
-    
-    for (let keys in numObj) {
-        if (numObj[keys].length >= 2) {
-            for (let i = 0; i < numObj[keys].length; i++) {
-                for (let j = 0; j < numObj[keys].length; j++) {
-                    if (j <= i) {
-                        continue;
-                    } else {
-                        if (Math.abs(numObj[keys][i] - numObj[keys][j]) <= k) {
-                            return true;
-                        }
-                    }
-                }
-            }
-        }
+    map.set(nums[i], i);
     }
-    
     return false;
     
 };
