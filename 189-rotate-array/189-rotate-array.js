1408f6e52e344ad2849e3240f168ef976f4916dc
Prith Jaganathan <58461828+prith98@users.noreply.github.com>
2022-03-05 13:13:24 -0800

    Time: 110 ms (77.35%), Space: 51.3 MB (55.73%) - LeetHub

--- 189-rotate-array/189-rotate-array.js
+++ 189-rotate-array/189-rotate-array.js
@@ -7,30 +7,19 @@ var rotate = function(nums, k) {
   
   let arrCopy = [...nums];
   
-  if (k < nums.length) {
-    let difference = nums.length - k;
-    for (let i = 0; i < nums.length; i++) {
-      if (k - i > 0) {
-        nums[i] = arrCopy[i + difference];
-      } else if (k === i) {
-        nums[i] = arrCopy[0]
-      } else if (k - i < 0) {
-        nums[i] = arrCopy[i - k]
-      }
-    }
+  if (k > nums.length) {
+    k = k % nums.length
   }
   
-  if (k > nums.length) {
-    k = k % nums.length;
-        let difference = nums.length - k;
-    for (let i = 0; i < nums.length; i++) {
-      if (k - i > 0) {
-        nums[i] = arrCopy[i + difference];
-      } else if (k === i) {
-        nums[i] = arrCopy[0]
-      } else if (k - i < 0) {
-        nums[i] = arrCopy[i - k]
-      }
+  let difference = nums.length - k;
+  
+  for (let i = 0; i < nums.length; i++) {
+    if (k - i > 0) {
+      nums[i] = arrCopy[i + difference];
+    } else if (k === i) {
+      nums[i] = arrCopy[0]
+    } else if (k - i < 0) {
+      nums[i] = arrCopy[i - k]
     }
   }
   
