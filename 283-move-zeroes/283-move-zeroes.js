7ce04057677b3955b0769893fe3f55cb61c8c4d4
Prith Jaganathan <58461828+prith98@users.noreply.github.com>
2022-03-08 08:16:04 -0800

    Time: 106 ms (77.01%), Space: 47.6 MB (5.07%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.js
+++ 283-move-zeroes/283-move-zeroes.js
@@ -4,22 +4,14 @@
  */
 var moveZeroes = function(nums) {
   
-  if (nums.length === 1) return nums;
-  
-  let sorted = true;
-  
-  for (let i = 0; i < nums.length - 1; i++) {
-    if (!nums[i] && nums[i + 1]) {
-      [nums[i], nums[i + 1]] = [nums[i + 1], nums[i]];
-      sorted = false;
+    let pointer = 0
+    for (let i in nums){
+        if (nums[i] !== 0){
+            [nums[pointer], nums[i]] = [nums[i], nums[pointer]]
+            pointer++
+        }
     }
-  }
-  
-  if (sorted) {
-    return nums;
-  } else {
-    moveZeroes(nums)
-  }
+    return nums
   
   
 };
