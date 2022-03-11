6e4b8d6201026a473fd09ddcc07a93fc3a225c59
Prith Jaganathan <58461828+prith98@users.noreply.github.com>
2022-03-11 10:33:51 -0800

    Time: 175 ms (21.11%), Space: 51.3 MB (57.74%) - LeetHub

--- 189-rotate-array/189-rotate-array.js
+++ 189-rotate-array/189-rotate-array.js
@@ -5,21 +5,19 @@
  */
 var rotate = function(nums, k) {
   
-  let arrCopy = [...nums];
+  let copy = [...nums]
   
   if (k > nums.length) {
     k = k % nums.length
+  } else if (k === nums.length) {
+    return nums;
   }
   
-  let difference = nums.length - k;
-  
   for (let i = 0; i < nums.length; i++) {
-    if (k - i > 0) {
-      nums[i] = arrCopy[i + difference];
-    } else if (k === i) {
-      nums[i] = arrCopy[0]
-    } else if (k - i < 0) {
-      nums[i] = arrCopy[i - k]
+    if (i < k) {
+      nums[i] = copy[nums.length - k + i]
+    } else {
+      nums[i] = copy[i - k]
     }
   }
   
