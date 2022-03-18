aae069bd3a211afbc5733fadd583cac6e4a3be14
Prith Jaganathan <58461828+prith98@users.noreply.github.com>
2022-03-17 17:41:56 -0700

    Time: 111 ms (24.61%), Space: 45.4 MB (22.91%) - LeetHub

--- 704-binary-search/704-binary-search.js
+++ 704-binary-search/704-binary-search.js
@@ -5,26 +5,20 @@
  */
 var search = function(nums, target) {
     
-
-    let low = 0;
-    let high = nums.length - 1;
-    
-    while (low <= high) {
-        
-        let midPoint = Math.floor((low + high) / 2);
-        if (nums[midPoint] === target) {
-            return midPoint
-        } else if (nums[midPoint] > target) {
-            high = midPoint - 1
-        } else if (nums[midPoint] < target) {
-            low = midPoint + 1;
-        }
-        
+  let low = 0;
+  let high = nums.length - 1;
+  
+  while (low <= high) {
+    let mid = Math.floor((low + high) / 2);
+    if (nums[mid] === target) {
+      return mid;
+    } else if (nums[mid] > target) {
+      high = mid - 1;
+    } else if (nums[mid] < target) {
+      low = mid + 1;
     }
-    
-    
-    return -1;
-    
-    
+  }
+  
+  return -1;
     
 };
