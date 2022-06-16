1d83e367fc52cf757f97df1362053df1da95211e
Prith Jaganathan <58461828+prith98@users.noreply.github.com>
2022-06-15 20:59:58 -0700

    Time: 118 ms, Memory: 51.9 MB - LeetHub

--- best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
+++ best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -3,15 +3,13 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    
+
     let maxProfit = 0;
     let minPrice = prices[0];
     
     for (let i = 1; i < prices.length; i++) {
         let currentPrice = prices[i];
-        if (currentPrice < minPrice) {
-            minPrice = currentPrice;
-        }
+        minPrice = Math.min(minPrice, currentPrice);
         maxProfit = Math.max(maxProfit, currentPrice - minPrice);
     }
     
